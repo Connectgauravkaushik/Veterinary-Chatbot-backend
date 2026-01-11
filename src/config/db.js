87039acdbcea28cd2e5e7a942a2ab78bdff8e936
Gaurav Kaushik <Connectgauravkaushik>
2026-01-12 00:09:13 +0530

    updated config/db

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://connectgauravkaushik_db_user:73dYdp9vCbF73KMU@cluster0.hhnkub6.mongodb.net/")
+    await mongoose.connect(process.env.MONGO_URI);
+ 
 };
 
 module.exports = connectDB;
