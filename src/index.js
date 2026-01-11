b4f7c3daaf69d6d1bc8cd9207a07b33b8c4a076b
Gaurav Kaushik <Connectgauravkaushik>
2026-01-12 00:12:54 +0530

    updated config/db

--- src/index.js
+++ src/index.js
@@ -1,23 +1,29 @@
 const express = require("express");
 const connectDB = require("./config/db");
-const app = express();
+const cors = require("cors");
 require("dotenv").config();
+
+const app = express();
 const PORT = process.env.PORT || 5000;
 
 const chatRoutes = require("./routes/chat.routes");
 const conversationRoutes = require("./routes/conversation.routes");
 
-app.use(express.json());
+app.use(cors());           
+app.use(express.json());    
 
-app.use("/api/chat" ,chatRoutes)
+
+app.use("/api/chat", chatRoutes);
 app.use("/api/conversations", conversationRoutes);
+
+/* Start server first (Render wants open port fast) */
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`API running on port ${PORT}`);
+});
 
 connectDB()
   .then(() => {
     console.log("Database connected successfully");
-    app.listen(PORT, () => {
-      console.log(`API running on port ${PORT}`);
-    });
   })
   .catch((error) => {
     console.error("Database connection failed:", error.message);
